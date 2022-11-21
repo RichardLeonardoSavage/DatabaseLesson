8d4a219a426ddc0b99edabb9936393617480f336
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-11-22 11:13:48 +1300

    init and added AllButtons component

---
+++ Components/AllButtons.js
@@ -0,0 +1,63 @@
+import {
+    Dimensions, // get the screen dimensions
+    StyleSheet, // CSS-like styles
+    Text, // Renders text
+    TouchableOpacity, // Handles row presses
+    Pressable, // Handles row presses
+   } from 'react-native';
+   import {React} from 'react';
+
+   export const TouchableOpacity = ({onPress, text}) => {
+    return(
+        <TouchableOpacity onPress={onPress} style={StyleSheet.button}>
+            <Text style={styles.text}>{Text}</Text>
+        </TouchableOpacity>
+    );
+   };
+
+   export const PressableButton = ({onPress, symbol}) => {
+    return(
+        <Pressable onPress={() => onPress(symbol)} style={styles.buttonPress}>
+            <Text style={styles.textPress}>{Symbol}</Text>
+        </Pressable>
+    );
+   };
+
+   //style for the buttons below
+
+   const screen = Dimensions.get('window');
+const buttonWidth = screen.width / 4;
+const styles = StyleSheet.create({
+ button: {
+ borderWidth: 3,
+ borderColor: '#64dded',
+ backgroundColor: '#ffffff',
+ flex: 1,
+ height: Math.floor(buttonWidth - 55),
+ alignItems: 'center',
+ justifyContent: 'center',
+ borderRadius: Math.floor(buttonWidth),
+ margin: 5,
+ },
+ text: {
+ color: '##060606',
+ fontSize: 24,
+ },
+ buttonPress: {
+ flex: 1,
+ justifyContent: 'center',
+ borderWidth: 3,
+ borderColor: '#23e25c',
+ backgroundColor: '#f06464',
+ alignItems: 'center',
+ alignSelf: 'stretch',
+ borderRadius: Math.floor(buttonWidth),
+ margin: 5,
+ },
+ textPress: {
+ color: 'white',
+ fontSize: 20,
+ fontWeight: 'bold',
+ textAlignVertical: 'center',
+ },
+});
