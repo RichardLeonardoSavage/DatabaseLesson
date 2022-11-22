b39744e4dc4530eb5c470dac35de0bca4ac0a969
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-11-22 13:05:16 +1300

    added imports and components to app.js

--- App.js
+++ App.js
@@ -1,5 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//imports
+import { SafeAreaView, ScrollView, StyleSheet, Text, View, ImageBackground, FlatList,}
+from 'react-native';
+import {React, useState} from 'react';
+import {CalcButtons} from './Components/calcbuttons';
+import {NumberButtons} from './Components/NumberButtons';
+import {DbButtons} from './Components/DbButtons';
+
+const App = () => {
+  const [calculation, setCalculation] = useState('');
+  const [DbDisplay, setDbDisplay] = useState([]);
+
+  const updateCalculation = value => {
+    setCalculation(calculation + String(value)); //add the value to the growing string
+    console.log('updateCalculation all', calculation);
+    //if you press = then ecaluate the calculation
+    if (value === '='){
+      let calc = calculation;
+      let answer = new Function('return ' + calc)();
+
+      setCalculation(calc + '='+ answer);
+    }
+    if (value === 'del'){
+      const result = calculation.slice(0,-1); //removes the last element from the string
+      console.log('updateCalculation DEL', result);
+      setCalculation(result);
+    }
+    };
+  
+
 
 export default function App() {
   return (
